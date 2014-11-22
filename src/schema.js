e6a4d96de0f41272b580af1f3055715a910b630f
James Wild <jameswilddev@outlook.com>
2014-11-22 08:58:35 +0000

    Renamed the namespace in schema.js to include no spaces and included the namespace in all internal references.  This seems to be necessary with a newer version of JsDoc.

--- src/schema.js
+++ src/schema.js
@@ -1,82 +1,83 @@
 /**
  * The JSON file format used by SUNRUSE.Studio to store a project.
- * @nameSpace SSP File Format 
+ * @nameSpace ProjectSchema
  */
 /**
  * A container object for all of the data held by a project.
  * @class Project 
- * @memberOf SSP File Format
+ * @memberOf ProjectSchema
  */
 /**
  * The tempo of the project, in beats per minute.  Should be greater than zero.
  * @member {Integer} tempo
- * @memberOf Project
+ * @memberOf ProjectSchema.Project
 /**
  * @member {Track} tracks
- * @memberOf Project
+ * @memberOf ProjectSchema.Project
  */ 
 /** 
- * A set of {@link Loop}s in a {@link Project}, with a description of how they 
- * should be synthesized.
+ * A set of {@link ProjectSchema.Loop}s in a {@link ProjectSchema.Project}, with 
+ * a description of how they should be synthesized.
  * @class Track 
- * @memberOf SSP File Format
+ * @memberOf ProjectSchema
  */
 /**
- * The {@link Loop}s this {@link Track} contains; these should not overlap when 
- * repeats are taken into account.
+ * The {@link ProjectSchema.Loop}s this {@link ProjectSchema.Track} contains; 
+ * these should not overlap when repeats are taken into account.
  * @member {Loop} loops
- * @memberOf Track
+ * @memberOf ProjectSchema.Track
  */
 /**
- * A repeatable section of a {@link Track}.
+ * A repeatable section of a {@link ProjectSchema.Track}.
  * @class Loop  
- * @memberOf SSP File Format
+ * @memberOf ProjectSchema
  */
 /**
- * The start time of the {@link Loop}, in beats, from the beginning of the {@link Project}.
- * Should be greater than or equal to zero.
+ * The start time of the {@link ProjectSchema.Loop}, in beats, from the 
+ * beginning of the {@link ProjectSchema.Project}.  Should be greater than or 
+ * equal to zero.
  * @member {Integer} start
- * @memberOf Loop
+ * @memberOf ProjectSchema.Loop
  */
 /**
- * The duration of one repeat of the {@link Loop}, in beats.
+ * The duration of one repeat of the {@link ProjectSchema.Loop}, in beats.
  * Should be greater than zero.
  * @member {Integer} duration
- * @memberOf Loop
+ * @memberOf ProjectSchema.Loop
  */
 /**
- * The number of times to repeat the {@link Loop}.
+ * The number of times to repeat the {@link ProjectSchema.Loop}.
  * Should be greater than zero.
  * @member {Integer} repeats
- * @memberOf Loop
+ * @memberOf ProjectSchema.Loop
  */
 /**
- * The {@linkNote}s this {@link Loop} contains.  Repeated during playback the 
- * number of times specified by repeats.  Notes should not begin before the 
- * start of the {@link Loop}, not on or after the end; though they may extend 
- * beyond the end.
+ * The {@link ProjectSchema.Note}s this {@link ProjectSchema.Loop} contains.  
+ * Repeated during playback the number of times specified by repeats.  Notes 
+ * should not begin before the start of the {@link ProjectSchema.Loop}, not on 
+ * or after the end; though they may extend beyond the end.
  * @member {Note} notes
- * @memberOf Loop
+ * @memberOf ProjectSchema.Loop
  */
 /**
- * A single tone in a {@link Loop}
+ * A single tone in a {@link ProjectSchema.Loop}
  * @class Note
- * @memberOf SSP File Format
+ * @memberOf ProjectSchema
  */
 /**
  * The time, in hemidemisemiquavers (1/64th beats) from the start of the loop
  * at which to start playing a tone.
  * Should be greater than or equal to zero and less than the duration of the
- * parent {@link Loop}.
+ * parent {@link ProjectSchema.Loop}.
  * @member {Integer} start
- * @memberOf Note
+ * @memberOf ProjectSchema.Note
  */
 /**
  * The time, in hemidemisemiquavers (1/64th beats) from the start of the loop
  * at which to stop playing a tone.
  * Should be greater than start.
  * @member {Integer} end
- * @memberOf Note
+ * @memberOf ProjectSchema.Note
  */
 /**
  * The note number in the Western scale at which to play, where zero is C0
@@ -84,6 +85,6 @@
  * inclusive.  To calculate the frequency of a note, use 
  * 16.35 * (2^12)^frequency.
  * @member {Integer} frequency
- * @memberOf Note
+ * @memberOf ProjectSchema.Note
  */
  
